a0ed3ef8199ce817c16f3edd803d28ee7ec789b4
blackpantech <pantech@blackpantech.com>
2025-02-28 16:35:36 +0100

    config and libs imports

---
+++ src/main/java/com/blackpantech/leitner_system_automaton/LeitnerSystemAutomatonApplication.java
@@ -0,0 +1,13 @@
+package com.blackpantech.leitner_system_automaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitnerSystemAutomatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitnerSystemAutomatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackpantech/leitner_system_automaton/LeitnerSystemAutomatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackpantech.leitner_system_automaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitnerSystemAutomatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
