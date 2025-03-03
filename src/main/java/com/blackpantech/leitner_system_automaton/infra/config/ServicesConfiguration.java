1c37f87e60c58c0626d83243e9ec02cccda925ed
blackpantech <pantech@blackpantech.com>
2025-03-03 16:44:47 +0100

    Services bean factories

---
+++ src/main/java/com/blackpantech/leitner_system_automaton/infra/config/ServicesConfiguration.java
@@ -0,0 +1,42 @@
+package com.blackpantech.leitner_system_automaton.infra.config;
+
+import com.blackpantech.leitner_system_automaton.domain.boxes.BoxesRepository;
+import com.blackpantech.leitner_system_automaton.domain.flashcards.FlashcardsRepository;
+import com.blackpantech.leitner_system_automaton.domain.flashcards.FlashcardsService;
+import com.blackpantech.leitner_system_automaton.domain.leitner_system.LeitnerSystemService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for bean factories
+ */
+@Configuration
+public class ServicesConfiguration {
+
+    /**
+     * Bean factory for FlashcardsService
+     *
+     * @param flashcardsRepository flashcards repository
+     *
+     * @return flashcards service bean
+     */
+    @Bean
+    public FlashcardsService flashcardsService(final FlashcardsRepository flashcardsRepository) {
+        return new FlashcardsService(flashcardsRepository);
+    }
+
+    /**
+     * Bean factory for LeitnerSystemService
+     *
+     * @param flashcardsRepository flashcards repository
+     * @param boxesRepository boxes repository
+     *
+     * @return Leitner system service bean
+     */
+    @Bean
+    public LeitnerSystemService leitnerSystemService(final FlashcardsRepository flashcardsRepository,
+                                                     final BoxesRepository boxesRepository) {
+        return new LeitnerSystemService(flashcardsRepository, boxesRepository);
+    }
+
+}
